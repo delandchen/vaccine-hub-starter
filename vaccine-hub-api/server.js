705b60a22227d053beed404dac98b9721975e531
Deland <chen.deland@gmail.com>
2022-06-28 18:19:27 -0400

    core features/connected backend to front

--- vaccine-hub-api/server.js
+++ vaccine-hub-api/server.js
@@ -1,6 +1,5 @@
 const app = require('./app');
-
-const PORT = process.env.PORT || 3001
+const { PORT } = require('./config.js')
 
 app.listen(PORT, () => {
     console.log("listening on port 3000...")
